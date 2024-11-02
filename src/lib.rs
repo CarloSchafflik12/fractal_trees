95d63ae66a8a5f7963b7c032cbc4c9040dc14911
Carlo Schafflik <carlo@hschafflik.de>
2024-11-02 16:37:34 +0100

    changed file structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 pub mod canvas;
+pub mod tree_gen;
 
 use canvas::Canvas;
 use clap::Parser;
-use std::error::Error;
+use tree_gen::{fractal_tree, fractal_tree_progress};
 
 #[derive(Parser)]
 pub struct Config {
@@ -31,7 +32,7 @@ pub struct Config {
     progress: bool,
 }
 
-pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: &Config) {
     let mut canvas = Canvas::new(config.resx, config.resy);
 
     // Progress mode
@@ -71,113 +72,4 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
         );
     }
     canvas.save("out.png");
-
-    Ok(())
-}
-
-fn fractal_tree(
-    canvas: &mut Canvas,
-    root_x: u32,
-    root_y: u32,
-    branch_len: f64,
-    branch_angle: f64,
-    max_index_conf: u32,
-    angle_conf: f64,
-    index: u32,
-) {
-    let root_next_x = (root_x as f64 + (branch_angle.to_radians().sin() * branch_len)) as u32;
-    let root_next_y = (root_y as f64 - (branch_angle.to_radians().cos() * branch_len)) as u32;
-
-    // Draw current branch
-    canvas.line(root_x, root_y, root_next_x, root_next_y);
-
-    let branch_angle_left = branch_angle - angle_conf;
-    let branch_angle_right = branch_angle + angle_conf;
-
-    if index == max_index_conf {
-        return;
-    }
-
-    // Draw next left branch
-    fractal_tree(
-        canvas,
-        root_next_x,
-        root_next_y,
-        branch_len * 0.8,
-        branch_angle_left,
-        max_index_conf,
-        angle_conf,
-        index + 1,
-    );
-
-    // Draw next right branch
-    fractal_tree(
-        canvas,
-        root_next_x,
-        root_next_y,
-        branch_len * 0.8,
-        branch_angle_right,
-        max_index_conf,
-        angle_conf,
-        index + 1,
-    );
-}
-
-fn fractal_tree_progress(
-    canvas: &mut Canvas,
-    root_x: u32,
-    root_y: u32,
-    branch_len: f64,
-    branch_angle: f64,
-    max_index_conf: u32,
-    angle_conf: f64,
-    index: u32,
-    lines_counter: &mut u32,
-    max_lines: u32,
-) {
-    let root_next_x = (root_x as f64 + (branch_angle.to_radians().sin() * branch_len)) as u32;
-    let root_next_y = (root_y as f64 - (branch_angle.to_radians().cos() * branch_len)) as u32;
-
-    // Draw current branch
-    print!(
-        "\r Progress: {:.2}%                                              ",
-        ((*lines_counter as f32 / max_lines as f32) * 100.0)
-    );
-    canvas.line(root_x, root_y, root_next_x, root_next_y);
-    *lines_counter += 1;
-
-    let branch_angle_left = branch_angle - angle_conf;
-    let branch_angle_right = branch_angle + angle_conf;
-
-    if index == max_index_conf {
-        return;
-    }
-
-    // Draw next left branch
-    fractal_tree_progress(
-        canvas,
-        root_next_x,
-        root_next_y,
-        branch_len * 0.8,
-        branch_angle_left,
-        max_index_conf,
-        angle_conf,
-        index + 1,
-        lines_counter,
-        max_lines,
-    );
-
-    // Draw next right branch
-    fractal_tree_progress(
-        canvas,
-        root_next_x,
-        root_next_y,
-        branch_len * 0.8,
-        branch_angle_right,
-        max_index_conf,
-        angle_conf,
-        index + 1,
-        lines_counter,
-        max_lines,
-    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::process;
-
 use clap::Parser;
 use fractal_tree::Config;
 
 fn main() {
     let config = Config::parse();
 
-    if let Err(e) = fractal_tree::run(&config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
+    fractal_tree::run(&config);
 }
